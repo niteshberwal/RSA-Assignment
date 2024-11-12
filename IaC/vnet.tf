80c11a586bb94c7b61cc1e131e5ad8498b71b190
niteshberwal <41788209+niteshberwal@users.noreply.github.com>
2024-11-12 16:06:31 +0530

    Create vnet.tf

---
+++ IaC/vnet.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "3-tier-network-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "3-tier-vnet"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "frontend_subnet" {
+  name                 = "frontend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "backend_subnet" {
+  name                 = "backend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
